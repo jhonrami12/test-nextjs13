5b9aad4b1ec5f338886cfc4fcc9f14b7451a31e8
jhonrami12 <jhonrami12@gmail.com>
2023-07-10 09:10:42 -0500

    some changes to try with two sources

--- components/VideoReactPlayer.jsx
+++ components/VideoReactPlayer.jsx
@@ -5,24 +5,64 @@ import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 export const VideoReactPlayer = () => {
-  const videoSources = ["/videos/vo2vo_EN.mp4", "/videos/vo2vo_ES.mp4"];
-  //   const videoSources = [
-  //     "https://www.youtube.com/watch?v=jNgP6d9HraI",
-  //     "https://www.youtube.com/watch?v=oUFJJNQGwhk",
-  //   ];
+  const videoSrc = "/videos/vo2vo_EN.mp4";
+  const audioSrc = "/audios/vo2vo_audio_ES.mp3";
+  const divStyle = {
+    position: 'relative',
+  };
 
-  const audioSources = [
-    {
-      src: "audio-url1.mp3",
-      type: "audio/mpeg",
-    },
-    {
-      src: "audio-url2.mp3",
-      type: "audio/mpeg",
-    },
-  ];
+  return (
+    <div className="relative audio-container bg-slate-600">
+       <style>
+        {`
+          audio::-webkit-media-controls-panel {
+            background-color: transparent;
+          }
 
-  return <ReactPlayer url={videoSources} playsinline controls />;
+          audio {
+            background-color: blue;
+          }
+
+          audio::-webkit-media-controls-panel {
+            background-color: transparent;
+          }
+          
+          audio::-webkit-media-controls-play-button {
+            color: red; /* Change to your desired color */
+          }
+          
+          audio::-webkit-media-controls-volume-slider {
+            background-color: transparent;
+          }
+          
+          /* For Firefox */
+          audio::-moz-range-thumb {
+            background-color: transparent;
+          }
+          
+          audio::-moz-range-track {
+            background-color: transparent;
+          }
+          
+          /* For IE and Edge */
+          audio::-ms-track {
+            background-color: transparent;
+          }
+          
+          audio::-ms-fill-lower {
+            background-color: transparent;
+          }
+          
+          audio::-ms-fill-upper {
+            background-color: transparent;
+          }
+
+        `}
+      </style>
+      <ReactPlayer url={videoSrc} controls />
+      <ReactPlayer url={audioSrc} style={divStyle} controls />     
+    </div>
+  );
 };
 
 // config={{
